5b5c3970baa80742ceaa45c6e780a1015b431221
Ben <45116438+benmacmillan@users.noreply.github.com>
2019-01-13 11:56:30 +0800

    Initial commit

---
+++ quiz.py
@@ -0,0 +1,25 @@
+import csv
+import random
+#Import and list questions from data.csv
+quiz = dict(csv.reader(open("data.csv", encoding='utf-8')))
+quizList = list(quiz.keys())
+
+print ("Python Kana Quiz")
+print ("respond to the kana with the correct latin character")
+print ("answer 'quit' to end")
+
+while True:
+	question = random.choice(quizList)
+	answer = quiz[question]
+
+	print (question)
+	questionAsk = input()
+
+	if questionAsk == (answer):
+		print("correct")
+		continue
+	elif questionAsk == ("quit"):
+		exit() 
+	else:
+		print("incorrect")
+		continue
