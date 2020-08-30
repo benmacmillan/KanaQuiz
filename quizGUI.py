52769753656a08b9644d794cb1e3493501f8ed4a
Ben <45116438+benmacmillan@users.noreply.github.com>
2020-08-30 09:40:07 +0800

    window icon
    added icon to the gui version of program. Changed font from courier to MS Gothic.

--- quizGUI.py
+++ quizGUI.py
@@ -6,6 +6,8 @@ import random
 root = Tk()
 root.title("Kana Quiz")
 root.geometry("300x200")
+root.iconbitmap('ico.ico')
+root.resizable(width=False, height=False)
 # Import and list questions from data.csv
 quiz = dict(csv.reader(open("data.csv", encoding='utf-8')))
 quizList = list(quiz.keys())
@@ -14,7 +16,7 @@ question = random.choice(quizList)
 answer = quiz[question]
 # Kana label display
 k_label = Label(text=question)
-k_label.config(font=("Courier", 60))
+k_label.config(font=("MS Gothic", 60))
 k_label.pack(pady=30)
 # Answer box
 a = Entry(root)
